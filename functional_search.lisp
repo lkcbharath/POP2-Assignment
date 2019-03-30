a49f5722204688b4792d9bdd4cda6a6385a40f5a
Bharath <lkcbharath@gmail.com>
2019-03-30 18:34:47 +0530

    LISP program fixed

--- functional_search.lisp
+++ functional_search.lisp
@@ -2,5 +2,14 @@
   (loop for pos from 0 and element in list
         when (eql item element)
           do (return pos)))
- 
-(write (linear-search '(2 3 5 7 11 13 17 19) 19))
+
+(format t "Enter the list of integers: ")
+(setq list (read-from-string (concatenate 'string "(" (read-line) ")")))
+
+(format t "Enter the integer to search for: ")
+(setq item (read-from-string (concatenate 'string (read-line))))
+
+(setq result (linear-search list item))
+(format t (concatenate 'string "Position of integer in array (0-indexed): " (write-to-string result)))
+
+;; (write (linear-search '(2 3 5 7 11 13 17 19) 19))
--- imperative_search.c
+++ imperative_search.c
@@ -1,29 +1,28 @@
 #include <stdio.h> 
 
-int binarySearch(int arr[], int l, int r, int x) 
+int search(int arr[], int n, int x) 
 { 
-	while (l <= r) { 
-		int m = l + (r - l) / 2; 
-		if (arr[m] == x) 
-			return m; 
-		if (arr[m] < x) 
-			l = m + 1; 
-		else
-			r = m - 1; 
-	}
+	int i; 
+	for (i = 0; i < n; i++) 
+		if (arr[i] == x) 
+			return i; 
 	return -1; 
 } 
 
-int main(void) 
-{ 
-	int arr[] = { 2, 3, 4, 10, 40 }; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
-	int x = 10; 
-	int result = binarySearch(arr, 0, n - 1, x); 
-	(result == -1) ? printf("Element is not present"
-							" in array") 
-				: printf("Element is present at "
-							"index %d", 
+int main(void) { 
+	int n,i,x;
+	printf("Enter the number of elements in the array: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the elements in the array:");
+	for(i=0;i<n;++i){
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter the element to search for in the array:");
+	scanf("%d",&x);
+	int result = search(arr, n, x); 
+	(result == -1) ? printf("Element is not present in array") 
+				: printf("Element is present at index %d", 
 							result); 
 	return 0; 
 } 
